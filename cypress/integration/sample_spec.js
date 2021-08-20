32757bc4a1fef8f8418c963694668f974daab37c
Marc Bakker <webdev@marcbakker.com>
2021-08-20 22:45:36 +0200

    Add data-cy selectors for Cypress test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,8 +1,11 @@
-describe('test Users', () => {
-	it('visit the Angular App', () => {
+describe('test login', () => {
+	it('Log in Angular App', () => {
 		cy.visit('http://localhost:4200')
 		cy.login('test@user.net', 't35tus3r')
-		cy.contains('users works!', { matchCase: true })
-		cy.get('div.user-name:contains("Clementin")').should('have.length', 2)
+		cy.get("[data-cy='Log out']").should('be.visible')
+		cy.get("[data-cy='Toon Photos']").should('be.visible')
+		cy.get("[data-cy='Toon Albums']").should('be.visible')
+		cy.get("[data-cy='Toon Users']").should('be.visible')
+		cy.get("[data-cy='Toon Posts']").should('be.visible')
 	})
 })
